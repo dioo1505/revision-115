a3ea803a4545d2103a8bebeb29cd311e1d69919e
dioo1505 <diyorakenjaboyeva1702@gmail.com>
2025-12-10 14:46:24 +0500

    added

--- js/index.js
+++ js/index.js
@@ -6,11 +6,12 @@ let obj = {
 };
 
 let result = Object.values(obj);
-let longWord = result.reduce((acc, curr) => acc.length < curr.length ? acc : curr);
+let longWord = result.reduce((acc, curr) =>
+  acc.length < curr.length ? acc : curr
+);
 console.log(longWord);
 
-
-let xavo = "sovuq"
+let xavo = "sovuq";
 // if(xavo == "sovuq"){
 //   console.log("Kiyin iplos");
 // } else{
@@ -18,3 +19,16 @@ let xavo = "sovuq"
 // }
 
 console.log(xavo == "issiq" ? "🤣" : "kiyin🥶");
+
+// Objectdagi sonlarni 2 barobarga oshirib yangi object yarating
+let numberObj = {
+  a: 10,
+  b: "hi",
+  c: 3,
+  d: true,
+};
+
+let ress = Object.values(numberObj)
+let resss = ress.filter((item)=>typeof item == "number");
+console.log(resss.map((item)=> item + 2));
+
